c1ece8f457594e705916ca8b6f7e6464236350fa
Sergo <114465482+lis999@users.noreply.github.com>
2023-03-26 23:45:34 -0700

    Delete strip & title with lamda.py

--- Codewars/strip & title with lamda.py
+++
@@ -1,5 +0,0 @@
-# Combine first name and last name into a single "Full name"
-
-full_name = lambda fn, ln: fn.strip().title() + ' ' + ln.strip().title()
-
-print(full_name('     leonard', 'REEPLE'))
